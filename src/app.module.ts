a2d406d00808cd21c166c2647cb3205cedf7fd67
NecroPrism4 <alexhid14@gmail.com>
2023-11-12 12:20:56 -0700

    a

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TenantModule } from './models/tenant/tenant.module';
-import { PropertyModule } from './models/property/property.module';
-import { PaymentModule } from './models/payment/payment.module';
 import { LeaseModule } from './models/lease/lease.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PaymentModule } from './models/payment/payment.module';
+import { PropertyModule } from './models/property/property.module';
+import { TenantModule } from './models/tenant/tenant.module';
 
 @Module({
   imports: [
